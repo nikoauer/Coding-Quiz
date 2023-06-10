436edebbbc0e570f89eca85d138eaa3635bf3bc0
Niko Auer <nikoauer26@gmail.com>
2023-06-10 17:34:15 +1000

    added a timer to the css and htm files

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,15 +2,32 @@
 var startQuiz = document.getElementById('startQuiz')
 var infoBox = document.getElementById('infoBox')
 var quizBox = document.getElementById('quizBox')
+var timer = document.getElementById('timer')
 
 //listens for start button click
 startQuiz.addEventListener('click', startTest)
 
 //sets the quiz box display to none to hide it until test starts
 quizBox.style.display = "none";
+
+var secondsLeft = 60;
+
 //start the test button.
 //turns on and off the start information and the quiz box
 function startTest() {
     infoBox.style.display = "none";
     quizBox.style.display = "";
+    setTimer();
     }
+
+// this the timing function
+function setTimer() {
+    var clock = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(clock);
+        }
+    }, 1000);
+}
